d546fb551da271c9da8db77368df670e0e21be63
tankedshoe <tankedshoe@gmail.com>
2018-01-08 08:54:19 -0700

    Setup a bitty of the Panel and the Frame.

--- src/pokemon/controller/PokemonController.java
+++ src/pokemon/controller/PokemonController.java
@@ -1,12 +1,45 @@
 package pokemon.controller;
 
 import pokemon.model.*;
+import pokemon.view.*;
 import java.util.List;
 import java.util.ArrayList;
 
 public class PokemonController
 {
 	private List<Pokemon> pokedex;
+	private PokemonFrame appFrame;
+	
+	public void Pokemon()
+	{
+		this.pokedex = new ArrayList<Pokemon>();
+		appFrame = new PokemonFrame(this);
+		
+		buildPokedex();
+	}
+	
+	private void buildPokedex()
+	{
+		pokedex.add(new Antima());
+		pokedex.add(new Blusha());
+		pokedex.add(new Buggle());
+		pokedex.add(new Copperus());
+		pokedex.add(new FlipperFish());
+		pokedex.add(new Metacular());
+		
+	}
+	
+	public String [] convertPokedex()
+	{
+		String[] names = new String [pokedex.size()];
+		
+		for (int index = 0; index < pokedex.size(); index++)
+		{
+			names[index] = pokedex.get(index).getName();
+		}
+		
+		return names;
+	}
 	
 	public List<Pokemon> getPokedex()
 	{
--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -3,7 +3,9 @@ package pokemon.view;
 import javax.swing.*;
 import pokemon.controller.*;
 
-public class PokemonPanel
+import java.awt.Color;
+
+public class PokemonPanel extends JPanel
 {
 	private PokemonController appController;
 	private SpringLayout layout;
@@ -35,7 +37,7 @@ public class PokemonPanel
 	private JPanel thirdType;
 	private JPanel fourthType;
 	
-	public PokemonPanel()
+	public PokemonPanel(PokemonController appController)
 	{
 		super();
 		this.appController = appController;
@@ -45,7 +47,7 @@ public class PokemonPanel
 		this.nameLabel = new JLabel("Default Name");
 		this.numberLabel = new JLabel("0");
 		this.evolvableLabel = new JLabel("No Modifier");
-		this.iconLabel = new JLabel("Default Icon");
+		this.iconLabel = new JLabel("Pokemon", new ImageIcon(getClass().getResource("/pokemon/view/images/default.png")), JLabel.CENTER);
 		
 		this.evolvableBox = new JCheckBox();
 		this.nameField = new JTextField(20);
@@ -54,10 +56,22 @@ public class PokemonPanel
 		this.healthField = new JTextField(20);
 		this.modifierField = new JTextField(20);
 		
-		this.descriptionArea = new JTextArea(10, 25);
-		this.typeArea = new JTextArea(10, 25);
+		this.descriptionArea = new JTextArea(5, 10);
+		this.typeArea = new JTextArea(4, 15);
 				
+		this.saveButton = new JButton("save");
+		this.clearButton = new JButton("clear");
+		this.pokedexDropdown = new JComboBox();
 		
+		this.firstType = new JPanel();
+		this.secondType = new JPanel();
+		this.thirdType = new JPanel();
+		this.fourthType = new JPanel();
+		
+		this.layout = new SpringLayout();
+		
+		setupComboBox();
+		setupTypePanels();
 		setupPanel();
 		setupLayout();
 		setupListeners();
@@ -73,14 +87,71 @@ public class PokemonPanel
 		modifierField.setText(appController.getPokedex().get(index).getEnhancementModifier() + "");
 	}
 	
+	private void setupComboBox()
+	{
+		DefaultComboBoxModel pokemonModel = new DefaultComboBoxModel(appController.convertPokedex());
+		pokedexDropdown.setModel(pokemonModel);
+	}
+	
 	private void setupPanel()
 	{
+		this.setLayout(layout);
+		
+		this.setBackground(Color.RED);
 		this.add(healthLabel);
+		this.add(attackLabel);
+		this.add(nameLabel);
+		this.add(numberLabel);
+		this.add(evolvableLabel);
+		this.add(iconLabel);
+		this.add(evolvableBox);
+		this.add(nameField);
+		this.add(numberField);
+		this.add(attackField);
+		this.add(healthField);
+		this.add(modifierField);
+		this.add(descriptionArea);
+		this.add(typeArea);
+		this.add(saveButton);
+		this.add(clearButton);
+		this.add(pokedexDropdown);
+		this.add(firstType);
+		this.add(secondType);
+		this.add(thirdType);
+		this.add(fourthType);
+		
 	}
 	
 	private void setupLayout()
 	{
-		
+		layout.putConstraint(SpringLayout.WEST, numberField, 301, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.EAST, numberField, -10, SpringLayout.EAST, this);
+		layout.putConstraint(SpringLayout.NORTH, iconLabel, 52, SpringLayout.SOUTH, saveButton);
+		layout.putConstraint(SpringLayout.WEST, iconLabel, 10, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.NORTH, descriptionArea, 0, SpringLayout.NORTH, iconLabel);
+		layout.putConstraint(SpringLayout.EAST, descriptionArea, 0, SpringLayout.EAST, nameField);
+		layout.putConstraint(SpringLayout.SOUTH, evolvableBox, -6, SpringLayout.NORTH, modifierField);
+		layout.putConstraint(SpringLayout.EAST, evolvableBox, -26, SpringLayout.EAST, this);
+		layout.putConstraint(SpringLayout.NORTH, numberLabel, 5, SpringLayout.NORTH, modifierField);
+		layout.putConstraint(SpringLayout.EAST, numberLabel, -28, SpringLayout.WEST, modifierField);
+		layout.putConstraint(SpringLayout.NORTH, healthLabel, 5, SpringLayout.NORTH, nameField);
+		layout.putConstraint(SpringLayout.EAST, healthLabel, -27, SpringLayout.WEST, nameField);
+		layout.putConstraint(SpringLayout.NORTH, evolvableLabel, 5, SpringLayout.NORTH, attackField);
+		layout.putConstraint(SpringLayout.WEST, evolvableLabel, 0, SpringLayout.WEST, attackLabel);
+		layout.putConstraint(SpringLayout.NORTH, attackLabel, 5, SpringLayout.NORTH, healthField);
+		layout.putConstraint(SpringLayout.EAST, attackLabel, 0, SpringLayout.EAST, iconLabel);
+		layout.putConstraint(SpringLayout.NORTH, nameLabel, 5, SpringLayout.NORTH, numberField);
+		layout.putConstraint(SpringLayout.EAST, nameLabel, -5, SpringLayout.WEST, numberField);
+		layout.putConstraint(SpringLayout.SOUTH, modifierField, -6, SpringLayout.NORTH, healthField);
+		layout.putConstraint(SpringLayout.EAST, modifierField, 0, SpringLayout.EAST, nameField);
+		layout.putConstraint(SpringLayout.SOUTH, healthField, -6, SpringLayout.NORTH, attackField);
+		layout.putConstraint(SpringLayout.EAST, healthField, 0, SpringLayout.EAST, nameField);
+		layout.putConstraint(SpringLayout.SOUTH, attackField, -10, SpringLayout.SOUTH, this);
+		layout.putConstraint(SpringLayout.EAST, attackField, -10, SpringLayout.EAST, this);
+		layout.putConstraint(SpringLayout.WEST, nameField, 0, SpringLayout.WEST, numberField);
+		layout.putConstraint(SpringLayout.EAST, nameField, 139, SpringLayout.WEST, numberField);
+		layout.putConstraint(SpringLayout.NORTH, nameField, 6, SpringLayout.SOUTH, numberField);
+		layout.putConstraint(SpringLayout.NORTH, numberField, 10, SpringLayout.NORTH, this);
 	}
 	
 	private void setupListeners()
